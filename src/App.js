d537c3f624c1881e38ec50b3bc8a81a833e19eb0
Andrey Martynov <kubris@mail.ru>
2023-12-20 13:49:21 +0300

    l18 Add Dialog-component, rebuilt page-structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import cls from './App.module.css';
+import './App.css';
 import Header from './components/Header/Header.jsx';
 import Aside from "./components/Aside/Aside";
 import Main from "./components/Main/Main";
+import Dialogs from "./components/Dialogs/Dialogs";
 import Footer from "./components/Footer/Footer";
 const App = () => {
 	return (
-		<div className={cls.pageWrapper}>
+		<div className="page-wrapper">
 			<Header />
 			<Aside />
-			<Main />
+			<main className="main">
+				{/*<Main />*/}
+				<Dialogs />
+			</main>
 			<Footer />
 		</div>
 	)
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,9 +3,7 @@ import Profile from "../Profile/Profile";
 import cls from './Main.module.css';
 const Main = () => {
     return (
-        <main className={cls.main}>
-            <Profile />
-        </main>
+        <Profile />
     );
 }
 
